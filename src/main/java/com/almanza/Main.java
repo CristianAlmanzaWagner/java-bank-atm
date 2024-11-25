23513f850e1c18216ee31905848ea52913322f63
Cristian Almanza <cristian.almanzawagner@gmail.com>
2024-11-25 15:57:10 +0100

    Se agrego nueva funcionalidad

--- src/main/java/com/almanza/Main.java
+++ src/main/java/com/almanza/Main.java
@@ -2,11 +2,6 @@ package com.almanza;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        try {
-            int result = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("No se puede dividir por cero");
-        }
+        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
     }
 }
